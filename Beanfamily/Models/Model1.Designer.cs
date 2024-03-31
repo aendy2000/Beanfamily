65dce862f532dbb60cf1845f736cc8240a1f8f3d
DangVanTuan <dnguyenhoang94@gmail.com>
2024-03-31 13:28:57 +0700

    Update data

--- Beanfamily/Models/Model1.Designer.cs
+++ Beanfamily/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\ProjectProgaming\Beanfamily\Beanfamily\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ProjectDevelopment\Beanfamily\aendy2000\Beanfamily\Beanfamily\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
